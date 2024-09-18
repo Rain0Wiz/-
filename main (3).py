2a968c5e6fe136714014dfbd15e3de1ef91cf4ca
Rain0Wiz <raindow0wizard@gmail.com>
2024-09-18 11:06:53 +0500

    Наследование
    новый код

--- main (3).py
+++ main (3).py
@@ -1,7 +1,29 @@
-print('Hello world!') 
+# Ассоциация - наследование при котором один класс является полем другого класса(Целое состоит из частей)
+# ассоциация-АГРЕГАЦИЯ - в UML (Унифицированный язык моделирования) - линия с незакрашенным ромбом(часть удалена объект-целое остаётся)
+class Salary:
+    def __init__(self, pay):
+        self.pay = pay # оплата за месяц
+    # метод вывода зарплаты за год
+    @property
+    def getSummOfYear(self):
+        return int(self.pay*12)
 
+class Worker:
+    def __init__(self, pay, bonus, name):
+        self.pay = pay
+        self.bonus = bonus
+        self.name = name
 
-class NewRoot:
-    def __init__(self, params):
-        self.params = params
+    # метод - выводит зарплату сотрудника за год вместе с примией
+    def getSummOfYearForWorker(self):
+        return f"Зарплата за год вместе с примией у сотрудника {self.name} состовляет {self.bonus + self.pay.getSummOfYear}"
+
+# объект - зарплата за месяц
+salary_of_month = Salary(20000)
+print(salary_of_month.getSummOfYear)
+# объект - работник
+sasha = Worker(salary_of_month, 20000, "Саша")
+print(sasha.getSummOfYearForWorker())
+        
             
+                    
